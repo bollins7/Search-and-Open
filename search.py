744eec4361eaa8a4044fc0b0d5590c8d4c592c00
Chris <37301037+bollins7@users.noreply.github.com>
2024-01-25 23:11:29 -0600

    Failed attempt
    This code only works on pypi.org but not on Google search. Will have to come back and tackle this another time. For now, v1.0 can search pypi.org but no Google

--- search.py
+++ search.py
@@ -4,8 +4,17 @@
 import requests, sys, webbrowser, bs4
 
 print('Searching...') # display text while loading the pages
-res = requests.get('https://www.google.com/search?q=' + ' '.join(sys.argv[1:]))
+res = requests.get("https://www.google.com/search?q=" + ' '.join(sys.argv[1:]), 'html.parser')
+print(sys.argv[1])
 res.raise_for_status()
 
-#TODO: Retrieve top results
-#TODO: Open tabs of search results automatically
+# Retrieve top results
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+# Open tabs of search results automatically
+linkElems = soup.select('.package-snippet')
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    urlToOpen = 'https://www.google.com' + linkElems[i].get('href')
+    print('Opening', urlToOpen)
+    webbrowser.open(urlToOpen)
